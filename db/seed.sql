ca6387b38150366972c30b2c1eaf7713a0db8c64
Arsteinberg <arsteinberg93@gmail.com>
2019-07-11 18:47:01 -0400

    getting an error, need to fix my handlebars files and make the buttons do what they are supposed to

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Good Burger", true);
